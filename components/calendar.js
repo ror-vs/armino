1bb1252ab6bffac7482417ae8ece3191038a9fee
Ali Hussain <ali.hussain@labk19.com>
2023-01-17 18:40:39 +0500

    calendar crud completed

--- components/calendar.js
+++ components/calendar.js
@@ -3,7 +3,6 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import { useRef, useState } from "react";
-import { createEventId, INITIAL_EVENTS } from "./event-utils";
 import CustomModal from "./Modal";
 import { Button, Modal, Input } from "antd";
 
@@ -12,39 +11,25 @@ const Calendar = () => {
   const [weekendsVisible, setWeekendsVisible] = useState(true);
   const [currentEvents, setCurrentEvents] = useState([]);
   const [currentEvent, setCurrentEvent] = useState("");
+  const [editEvent, setEditEvent] = useState("");
+
+  const [selectinfo, setSelectInfo] = useState("");
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [showEdit, setShowEdit] = useState(false);
-  const [deleteEvent, setDeleteEvent] = useState(false);
+
   const [title, setTitle] = useState("");
-  const [start, setStart] = useState("");
-  const [end, setEnd] = useState("");
 
   const handleDateSelect = (selectInfo) => {
-    console.log(selectInfo);
-    setStart(selectInfo.startStr);
-    setEnd(selectInfo.endStr);
-    setShowEdit(true);
-    if (title && start && end) {
-      setCurrentEvents((currentEvents) => [
-        ...currentEvents,
-        {
-          id: createEventId(),
-          title,
-          start,
-          end,
-          allDay: selectInfo.allDay,
-        },
-      ]);
-    }
-    console.log("current events", currentEvents);
+    setSelectInfo(selectInfo);
+    setIsModalOpen(true);
   };
 
   const handleEventClick = (clickInfo) => {
-    console.log(clickInfo.title, "clickInfo");
     if (clickInfo) {
-      setCurrentEvent(clickInfo.event);
-      setIsModalOpen(true);
+      setCurrentEvent(clickInfo);
+      setShowEdit(true);
+      setEditEvent(clickInfo.event.title);
     }
   };
 
@@ -64,41 +49,64 @@ const Calendar = () => {
   };
   function handleDeleteEvent() {
     if (currentEvent) {
-      let newEvents = currentEvents.filter((item) => {
-        item.id !== currentEvent.id;
-      });
-      setCurrentEvents(newEvents);
+      currentEvent.event.remove();
+      setShowEdit(false);
     }
   }
+
+  const handleAddEvent = () => {
+    let selectInfo = selectinfo;
+    let calendarApi = selectInfo.view.calendar;
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: Date.now(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+      setTitle("");
+      setIsModalOpen(!isModalOpen);
+    }
+  };
+  const handleUpdate = () => {
+    let selectInfo = selectinfo;
+    const calendarApi = selectInfo.view.calendar;
+    const currentEventApi = calendarApi.getEventById(currentEvent.event.id);
+
+    if (currentEventApi) {
+      currentEventApi.setProp("title", editEvent);
+      setShowEdit(!showEdit);
+    }
+  };
+
   return (
     <>
       <CustomModal
-        id={currentEvent.id}
-        title={currentEvent.title}
-        isModalOpen={isModalOpen}
-        setIsModalOpen={setIsModalOpen}
+        title={editEvent}
+        setTitle={setEditEvent}
+        isModalOpen={showEdit}
+        setIsModalOpen={setShowEdit}
         handleDeleteEvent={handleDeleteEvent}
-        handleDateSelect={handleDateSelect}
-        showEdit={showEdit}
-        setShowEdit={setShowEdit}
+        handleUpdate={handleUpdate}
       />
-      {showEdit && (
-        <Modal
-          title="Event Modal"
-          open={showEdit}
-          onOk={() => setShowEdit(false)}
-          onCancel={() => setShowEdit(false)}
-        >
-          <Input
-            style={{ marginBottom: "10px", maxWidth: "400px" }}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Enter Title"
-          />
-          <Button type="primary" onClick={handleDateSelect}>
-            Add Event
-          </Button>
-        </Modal>
-      )}
+
+      <Modal
+        title="Event Modal"
+        open={isModalOpen}
+        onOk={handleAddEvent}
+        onCancel={() => setIsModalOpen(false)}
+      >
+        <Input
+          value={title}
+          style={{ marginBottom: "10px", maxWidth: "400px" }}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter Title"
+        />
+      </Modal>
+
       <FullCalendar
         height={700}
         ref={calendarRef}
@@ -114,16 +122,12 @@ const Calendar = () => {
         selectMirror={true}
         dayMaxEvents={true}
         weekends={weekendsVisible}
-        events={currentEvents} // alternatively, use the `events` setting to fetch from a feed
+        events={currentEvents}
+        initialEvents={currentEvents} // alternatively, use the `events` setting to fetch from a feed// alternatively, use the `events` setting to fetch from a feed
         select={handleDateSelect}
         eventContent={renderEventContent} // custom render function
         eventClick={handleEventClick}
-        // eventsSet={handleEvents} // called after events are initialized/added/changed/removed
-        /* you can update a remote database when these fire:
-      eventAdd={function(){}}
-      eventChange={function(){}}
-      eventRemove={function(){}}
-      */
+        eventsSet={handleEvents}
       />
     </>
   );
