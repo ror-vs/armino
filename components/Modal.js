99ddf3a563c3b8048373d8177afb6f134d82120e
Ali Hussain <ali.hussain@labk19.com>
2023-01-17 19:33:16 +0500

    update modals

--- components/Modal.js
+++ components/Modal.js
@@ -1,4 +1,4 @@
-import { Button, Modal, Input } from "antd";
+import { Button, Modal, Input, Row, Col } from "antd";
 const CustomModal = ({
   isModalOpen,
   setIsModalOpen,
@@ -10,21 +10,37 @@ const CustomModal = ({
   return (
     <>
       <Modal
-        title="Event Modal"
+        title={`Edit Event`}
         open={isModalOpen}
-        onOk={handleUpdate}
-        onCancel={() => setIsModalOpen(false)}
+        footer={[
+          <Button
+            key="back"
+            type="primary"
+            danger
+            onClick={() => setIsModalOpen(false)}
+          >
+            Cancel
+          </Button>,
+          <Button key="submit" type="primary" onClick={handleUpdate}>
+            Update Event
+          </Button>,
+        ]}
       >
-        {/* <h3>{title}</h3> */}
-        <Input
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          style={{ marginBottom: "10px", maxWidth: "400px" }}
-        />
-        {/* <p>{time}</p> */}
-        <Button type="primary" onClick={handleDeleteEvent}>
-          delete event
-        </Button>
+        <Row gutter={16}>
+          <Col className="gutter-row" span={12}>
+            <Input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              style={{ marginBottom: "10px", maxWidth: "400px" }}
+            />
+          </Col>
+          <Col className="gutter-row" span={4}>
+            {/* <p>{time}</p> */}
+            <Button type="primary" danger onClick={handleDeleteEvent}>
+              delete event
+            </Button>
+          </Col>
+        </Row>
       </Modal>
     </>
   );
