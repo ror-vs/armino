193723fdea9567e0274c343b6d5f0bd1a001789c
Ali Hussain <ali.hussain@labk19.com>
2023-01-17 18:40:39 +0500

    calendar crud completed

--- components/Modal.js
+++ components/Modal.js
@@ -4,18 +4,21 @@ const CustomModal = ({
   setIsModalOpen,
   title,
   handleDeleteEvent,
+  handleUpdate,
+  setTitle,
 }) => {
   return (
     <>
       <Modal
         title="Event Modal"
         open={isModalOpen}
-        onOk={() => setIsModalOpen(false)}
+        onOk={handleUpdate}
         onCancel={() => setIsModalOpen(false)}
       >
         {/* <h3>{title}</h3> */}
         <Input
           value={title}
+          onChange={(e) => setTitle(e.target.value)}
           style={{ marginBottom: "10px", maxWidth: "400px" }}
         />
         {/* <p>{time}</p> */}
